9d467a92d56ed2659b3c68645be0b6db2c69cd6e
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-01-02 22:00:13 -0300

    Estudo componente com parametros/props

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Comp from './components/basicos/Primeiro'
-
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id='app'>
         <Comp></Comp>
+        <ComParametro 
+            titulo="Situação do Aluno" 
+            aluno="Pedro Silva" nota={9.3} />
 
+        <ComParametro 
+            titulo="Situação do Aluno" 
+            aluno="Maria" nota={9.9} />
     </div>,
     document.getElementById('root')
 )
