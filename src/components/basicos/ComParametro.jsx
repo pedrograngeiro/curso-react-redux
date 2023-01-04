eabbcb2c3fc07c35d4731b8e2418aab86e6914a8
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-01-04 17:19:50 -0300

    Add arredondar nota

--- src/components/basicos/ComParametro.jsx
+++ src/components/basicos/ComParametro.jsx
@@ -2,13 +2,14 @@ import React from "react"
 
 export default function ComParametro(props) {
     const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação'
+    const notaInt = Math.ceil(props.nota)
     return (
         <div>
             <h2>{ props.titulo }</h2>
             <p>
                 <strong>{ props.aluno } </strong>
                 tem nota
-                <strong> { props.nota }</strong></p>
+                <strong> { notaInt }</strong></p>
                 e está
                 <strog> { status } </strog>!
         </div>
--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import './index.css'
-import ReactDOM from 'react-dom'
-import React from 'react'
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
 
-import Comp from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
+import Comp from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 ReactDOM.render(
-    <div id='app'>
-        <Comp></Comp>
-        <ComParametro 
-            titulo="Situação do Aluno" 
-            aluno="Pedro Silva" nota={9.3} />
+  <div id="app">
+    <Comp></Comp>
+    <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
 
-        <ComParametro 
-            titulo="Situação do Aluno" 
-            aluno="Maria" nota={9.9} />
-    </div>,
-    document.getElementById('root')
-)
-
+    <ComParametro titulo="Situação do Aluno" aluno="Maria" nota={8.7} />
+  </div>,
+  document.getElementById("root")
+);
