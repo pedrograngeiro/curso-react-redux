896fe635f93c252bf12c208c154f02f24f46381a
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-01-04 17:34:54 -0300

    React.Frament ou elemento vazio, para remover a necessidade de cercamento de div

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 
 import Comp from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 ReactDOM.render(
   <div id="app">
@@ -11,6 +12,7 @@ ReactDOM.render(
     <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
 
     <ComParametro titulo="Situação do Aluno" aluno="Maria" nota={8.7} />
+    <Fragmento />
   </div>,
   document.getElementById("root")
 );
