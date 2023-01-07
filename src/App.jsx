b15e487aefc399035707d12b562fba11db38be9e
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-01-06 22:39:37 -0300

    Mudanças nas configurações do eslint e prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-import Comp from "./components/basicos/Primeiro";
+import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 
-export default function App(props) {
+export default function App() {
   return (
     <div id="app">
-      <Comp />
-      <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
-
-      <ComParametro titulo="Situação do Aluno" aluno="Maria" nota={8.7} />
+      <h1>Fundamentos react (Arrow)</h1>
       <Fragmento />
+      <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
+      <Primeiro />
     </div>
   );
 }
