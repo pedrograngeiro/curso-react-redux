8928ca436ad61a79fcacc71eed2681f4905c20c0
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-01-21 23:17:08 -0300

    Construnindo alguns componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import "./App.css";
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
@@ -7,13 +9,26 @@ import Aleatorio from "./components/basicos/Aleatorio";
 
 export default function App() {
   return (
-    <div id="app">
+    <div className="App">
       <h1>Fundamentos react (Arrow)</h1>
-      <Fragmento />
-      <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
-      <Primeiro />
 
-      <Aleatorio min={1} max={60} />
+      <div className="Cards">
+        <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+        <Aleatorio min={1} max={60} />
+        </Card>
+        
+        <Card titulo="#03 - Fragmento" color="#E94C6F">
+        <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+        <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
+        </Card>
+      
+        <Card titulo="#01 - Primeiro Componente" color="#588C73">
+        <Primeiro />
+        </Card>
+      </div>
     </div>
   );
 }
